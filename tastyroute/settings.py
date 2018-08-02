8ccfbc7bca35abac2a70001431abf3808089b87c
William Condori Quispe <williamccondori@gmail.com>
2018-08-02 13:15:31 -0500

    Se modifica exclusion y configuracion v2

--- tastyroute/settings.py
+++ tastyroute/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = '0sy(8wkdm1xeediml5v=2s3r$w8^15^e%zd9&bax7ud01+%uwc'
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
     'tastyroute.herokuapp.com'
 ]
 
@@ -116,14 +117,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
